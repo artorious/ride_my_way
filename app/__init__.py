62cb0df13e29be868ef1165685b04e6e6d9ad20c
Arthur Ngondo <ngondoarthur@gmail.com>
2018-06-26 10:25:15 +0300

    [Chore #158549332] Set-up config, Init and runner

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+""" Initalization file """
+
+from flask import Flask
+
+# Init the app
+app = Flask(__name__, instance_relative_config=True)
+
+# Load views
+from app import views
+
+# Load config file
+app.config.from_object('config')
+
---
+++ app/models.py
@@ -0,0 +1,5 @@
+""" Data representation for ride_my_way Web App
+
+    Holds routines for users to interact with the API
+"""
+
